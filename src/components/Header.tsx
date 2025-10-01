7e26ff64c26253775c3f36ba326b25097864b39c
이지은 <tori@ryanlab.kr>
2025-10-01 17:41:33 +0900

    Header html 작성

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const Header = () => {
+    const [isNavActive, setIsNavActive] = useState(false);
+    const [isMenuActive, setIsMenuActive] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsNavActive(!isNavActive);
+        setIsMenuActive(!isMenuActive);
+    };
+
+    return (
+        <header>
+            <div className="container">
+                <h1 className="logo">
+                    <a href="#">
+                        <img src="/nike/img/logo.png" alt="나이키 로고" />
+                    </a>
+                </h1>
+                <button
+                    className={isMenuActive ? "menu_btn active" : "menu_btn"}
+                    onClick={handleMenuClick}
+                >
+                    <span className="sr_only">메뉴 버튼</span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <nav className={isNavActive ? "active" : ""}>
+                    <h2 className="sr_only">메인 네비게이션</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Brand</a>
+                        </li>
+                        <li>
+                            <a href="#">Iconic</a>
+                        </li>
+                        <li>
+                            <a href="#">History</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
