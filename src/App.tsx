4ef09a433588d65a42417d7d8058d046acb6c1b8
이지은 <tori@ryanlab.kr>
2025-10-04 13:52:05 +0900

    확장자 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer.tsx";
+import Header from "./components/Header/Header.tsx";
+import Home from "./pages/Home/Home.tsx";
 
 function App() {
     return (
